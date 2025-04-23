54ed0686260cb3c22b48b7acb706af8792d4b98f
Fabian Rivera <frakxman@hotmail.com>
2025-04-23 15:23:03 -0500

    Enable CORS for local development in NestJS application

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
